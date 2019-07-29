7fdb5162c184e1bb86c9e1e933c59dbcb1e8199e
Ashique Hussain <ashique.hussain@intimetec.com>
2019-07-29 10:48:45 +0530

    Index.html added

---
+++ app.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', {
+        scope: '/*'
+    }).then(function(sw) {
+        // registration worked!
+        console.log('Success');
+    }).catch(function() {
+        // registration failed :(
+        console.log('Failtastic');
+    });
+}
---
+++ sw.js
@@ -0,0 +1,1 @@
+alert('Service Worker active');
